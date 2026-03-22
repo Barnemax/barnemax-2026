55be09d48eb452f61e2061770ebd8afd2c6de857
barnemax <maxime.etchebarne@gmail.com>
2026-03-22 19:01:19 +0100

    fix (seo): adding redirect from previous version

--- frontend/config/redirects.ts
+++ frontend/config/redirects.ts
@@ -1,6 +1,7 @@
 import type { NuxtConfig } from 'nuxt/schema'
 
 const redirects: NuxtConfig['routeRules'] = {
+  '/author/**': { redirect: { statusCode: 301, to: '/' } },
   '/credits': { redirect: { statusCode: 301, to: '/' } },
   '/project/**': { redirect: { statusCode: 301, to: '/projects/**' } },
 }
